fc0a943ba9f75b6b43bcb22b56935e5c9b0d3811
Luis Mastrangelo <luis@swirldslabs.com>
2025-10-04 03:23:12 +0200

    Restore create byte test
    Signed-off-by: Luis Mastrangelo <luis@swirldslabs.com>

--- tests/frontier/create/test_create_one_byte.py
+++ tests/frontier/create/test_create_one_byte.py
@@ -39,13 +39,12 @@ def test_create_one_byte(
     create_opcode: Op,
 ):
     """Run create deploys with single bytes for each byte."""
-
     initcode: dict[int, Bytecode] = {}
+
     # NOTICE CN config variable `consensus.handle.maxFollowingRecord` needs to be greater than 255 (default 50).
     # Otherwise this test fails with `MAX_CHILD_RECORDS_EXCEEDED` when creating too many contracts within the same transaction.
-    # This test generates initcode for **50** contracts to make the test pass without changing the CN configuration.
     # https://github.com/gkozyryatskyy/execution-spec-tests/issues/18
-    for byte in range(50):
+    for byte in range(256):
         initcode[byte] = Op.MSTORE8(0, byte) + Op.RETURN(0, 1)
     initcode_length = 10
 
